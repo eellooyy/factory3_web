2fd7d3984c235af71682b5461a1a1f9fd153181d
eellooyy <n825170@gmail.com>
2026-06-29 14:49:54 +0900

    수정

--- js/factory3_io_script.js
+++ js/factory3_io_script.js
@@ -5,8 +5,5 @@
     document.addEventListener('DOMContentLoaded', function() {
         Factory3Header.init({ idPrefix: 'Io' });
     });
-<<<<<<< HEAD
-})(); No newline at end of file
-=======
+
 })();
->>>>>>> parent of f4bbc71 (io 페이지 수정)
